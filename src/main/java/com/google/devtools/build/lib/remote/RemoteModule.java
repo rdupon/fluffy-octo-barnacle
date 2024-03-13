bae8dc4a3876144641dded5f198a5985c57b0a22
Chi Wang <chiwang@google.com>
2024-03-13 03:54:29 -0700

    Add --experimental_remote_output_service flag
    which accept an endpoint pointing to the local output service daemon.
    
    Since the feature is still WIP, Bazel exits and prints an error if this flag is set.
    
    Working towards #21630.
    
    Closes #21649.
    
    PiperOrigin-RevId: 615357528
    Change-Id: Ie4cc3a1636b1cf0e710d23883f6bb7b22de7d6c3

--- src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteModule.java
@@ -392,6 +392,14 @@ public final class RemoteModule extends BlazeModule {
       return;
     }
 
+    if (!Strings.isNullOrEmpty(remoteOptions.remoteOutputService)) {
+      throw createExitException(
+          "Remote Output Service is still WIP",
+          ExitCode.REMOTE_ERROR,
+          Code.REMOTE_EXECUTION_UNKNOWN);
+    }
+    remoteOutputService = new RemoteOutputService(env);
+
     if ((enableHttpCache || enableDiskCache) && !enableGrpcCache) {
       initHttpAndDiskCache(
           env, credentials, authAndTlsOptions, remoteOptions, digestUtil, executorService);
