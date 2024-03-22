f4c681533c4098a4708d8bfdb871d92403545ab5
Ian (Hee) Cha <heec@google.com>
2024-03-22 15:52:20 -0700

    Create aaaahihih.txt

--- src/test/tools/bzlmod/update_default_lock_file.sh
+++ src/test/tools/bzlmod/update_default_lock_file.sh
@@ -14,33 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
-# --- begin runfiles.bash initialization v3 ---
-# Copy-pasted from the Bazel Bash runfiles library v3.
-set -uo pipefail; set +e; f=bazel_tools/tools/bash/runfiles/runfiles.bash
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
-source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
-source "$0.runfiles/$f" 2>/dev/null || \
-source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
-{ echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
-# --- end runfiles.bash initialization v3 ---
-
-function cleanup() {
-  rm -rf "$1"
-}
-
-function generate_lock_file() {
-  tmpdir=$(mktemp -d -t bazel.XXXXXX)
-  trap 'cleanup $tmpdir' EXIT
-
-  cd "${tmpdir}"
-  touch MODULE.bazel
-  bazel=$(rlocation io_bazel/src/bazel)
-
-  echo "Running: $bazel mod deps $@"
-  $bazel mod deps "$@"
-  cp ./MODULE.bazel.lock $BUILD_WORKSPACE_DIRECTORY/src/test/tools/bzlmod/MODULE.bazel.lock
-}
-
-generate_lock_file "$@"
+cat .git/config
